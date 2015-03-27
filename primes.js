e6a79eb65caaed133a8626eeed47e54e471813f0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-03-26 23:43:23 -0400

    using ng-webworker to compute primes really quickly plus without needing an http server

--- primes.js
+++ primes.js
@@ -1,55 +1,61 @@
-function isPrime(n) {
-  var k;
-  var limit = Math.sqrt(n);
-  for (k = 2; k <= limit; k += 1) {
-    if (n % k === 0) {
-      return false;
+// can be used directly
+function primes(n) {
+  function isPrime(n) {
+    var k;
+    var limit = Math.sqrt(n);
+    for (k = 2; k <= limit; k += 1) {
+      if (n % k === 0) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-}
-console.assert(isPrime(1));
-console.assert(isPrime(2));
-console.assert(isPrime(3));
-console.assert(!isPrime(4));
-console.assert(isPrime(5));
-console.assert(!isPrime(6));
-console.assert(isPrime(7));
-console.assert(isPrime(37));
-console.assert(!isPrime(38));
+  console.assert(isPrime(1));
+  console.assert(isPrime(2));
+  console.assert(isPrime(3));
+  console.assert(!isPrime(4));
+  console.assert(isPrime(5));
+  console.assert(!isPrime(6));
+  console.assert(isPrime(7));
+  console.assert(isPrime(37));
+  console.assert(!isPrime(38));
 
-// finds Nth prime
-var foundPrimes = [];
-function findPrime(n) {
-  var k;
-  if (foundPrimes.length) {
-    k = foundPrimes[foundPrimes.length - 1] + 1;
-  } else {
-    k = 1;
-  }
-  while (foundPrimes.length < n) {
-    if (isPrime(k)) {
-      foundPrimes.push(k);
+  // finds Nth prime
+  var foundPrimes = [];
+  function findPrime(n) {
+    var k;
+    if (foundPrimes.length) {
+      k = foundPrimes[foundPrimes.length - 1] + 1;
+    } else {
+      k = 1;
     }
-    k += 1;
-  };
-  return foundPrimes[n - 1];
-}
-console.assert(findPrime(1) === 1);
-console.assert(findPrime(2) === 2);
-console.assert(findPrime(3) === 3);
-console.assert(findPrime(4) === 5);
-console.assert(findPrime(5) === 7);
+    while (foundPrimes.length < n) {
+      if (isPrime(k)) {
+        foundPrimes.push(k);
+      }
+      k += 1;
+    };
+    return foundPrimes[n - 1];
+  }
+  console.assert(findPrime(1) === 1);
+  console.assert(findPrime(2) === 2);
+  console.assert(findPrime(3) === 3);
+  console.assert(findPrime(4) === 5);
+  console.assert(findPrime(5) === 7);
 
-onmessage = function(e) {
-  var first = e.data.first;
-  var last = e.data.last;
-
-  var k, primes = [];
-  for (k = first; k < last; k += 1) {
-    var prime = findPrime(k + 2);
-    primes.push(prime);
+  function computePrimes(n) {
+    var k, primeNumbers = [];
+    for (k = 0; k < n; k += 1) {
+      var prime = findPrime(k + 1);
+      primeNumbers.push(prime);
+    }
+    return primeNumbers;
   }
 
-  postMessage(primes);
+  return computePrimes(n);
+}
+
+// can be run from web worker
+self.onmessage = function (e) {
+  self.postMessage(primes(e.data));
 };
